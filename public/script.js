df8bfabe30228240f1acf4479308f2d52784cdc0
Josh Williams <josh.r.williams@gmail.com>
2023-07-09 19:37:12 -0700

    added a form

---
+++ public/script.js
@@ -0,0 +1,15 @@
+function showform() {
+  // Get the div element
+  const div = document.getElementById("googleform");
+
+  // Add the CSS style
+  div.style.display = "block";
+}
+
+function hideform() {
+  // Get the div element
+  const div = document.getElementById("googleform");
+
+  // Add the CSS style
+  div.style.display = "none";
+}
