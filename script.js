ed3a155f0dbf6a6bb2bafd99bed83592ee965a3f
Josh Williams <josh.r.williams@gmail.com>
2024-05-22 21:53:16 -0700

    this should auto scroll now

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const consoleElement = document.getElementById('console');
 const consoleLines = [
     { text: ".", delay: 150 },
+    { text: ".. ", delay: 150 },
+    { text: "... ", delay: 150 },
+    { text: "....", delay: 150 },
+    { text: "Welcome to:", delay: 150 },
+    { text: "G.U.A.C. O.F.F. 2024", delay: 150 },
+    { text: "Booting system...", delay: 150 },
     { text: ".", delay: 150 },
+    { text: ".. ", delay: 150 },
+    { text: "... ", delay: 150 },
+    { text: "....", delay: 150 },
+    { text: "...", delay: 150 },
+    { text: "..", delay: 150 },
     { text: ".", delay: 150 },
-    { text: ".", delay: 150 },
-    { text: "Welcome to:", delay: 800 },
-    { text: "G.U.A.C. O.F.F. 2024", delay: 2500 },
-    { text: "Booting system...", delay: 2000 },
-    { text: ".", delay: 150 },
-    { text: ".", delay: 150 },
+    { text: "Welcome to:", delay: 150 },
+    { text: "DEBUG MODE ENABLED...", delay: 150 },
+    { text: "CONTENT WILL LOAD FAST FOR TESTING", delay: 150 },
+    { text: "....", delay: 150 },
+    { text: "...", delay: 150 },
+    { text: "..", delay: 150 },
     { text: ".", delay: 150 },
     { text: "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣶⣾⣿⣿⣷⢰⣆⠀⠀", delay: 100 },
     { text: "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⣿⣿⣿⣿⣿⣿⣿⡏⠀⢿⡄⠀", delay: 100 },
@@ -23,9 +34,9 @@ const consoleLines = [
     { text: "⠀⢸⣿⣿⣿⣷⣶⣶⣿⣿⣿⡿⠟⠋⠁⠀⠀⠀⠀⠀⣠⡾⠋⠀⠀⠀⠀⠀⠀⠀", delay: 100 },
     { text: "⠀⠀⠙⠿⠿⠿⠿⠟⠛⠉⠁⠀⠀⠀⠀⠀⠀⢀⣤⡾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀", delay: 100 },
     { text: "⠀⠀⠀⠀⠀⠒⠶⢶⣤⣤⣤⣤⣤⣤⣴⠶⠞⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀", delay: 100 },
-    { text: ".", delay: 50 },
-    { text: ".", delay: 50 },
-    { text: ".", delay: 50 },
+    { text: "\u00A0", delay: 50 },
+    { text: "\u00A0", delay: 50 },
+    { text: "\u00A0", delay: 50 },
     { text: "Grabbing avocados...", delay: 200 },
     { text: "Sharpening knife...", delay: 200 },
     { text: "Dicing onion...", delay: 200 },
@@ -38,13 +49,23 @@ const consoleLines = [
     { text: ".", delay: 50 },
     { text: ".", delay: 50 },
     { text: ".", delay: 50 },
-    { text: "ENTER SECRET INGREDIENT...", delay: 2000 },
-    { text: "***********", delay: 2000 },
+    { text: "ENTER SECRET INGREDIENT...", delay: 100 },
+    { text: "***********", delay: 100 },
     { text: ".", delay: 50 },
     { text: ".", delay: 50 },
     { text: ".", delay: 50 },
-    { text: "Testing taste.", delay: 200 },
-    { text: "Type any key to continue...", delay: 1000 },
+    { text: "Testing taste.", delay: 100 },
+    { text: "September 14, 2024", delay: 100 },
+    { text: "You know the rules.", delay: 100 },
+    { text: "\u00A0", delay: 50 },
+    { text: "\u00A0", delay: 50 },
+    { text: "\u00A0", delay: 50 },
+    { text: "\u00A0", delay: 50 },
+    { text: "\u00A0", delay: 50 },
+    { text: "\u00A0", delay: 50 },
+    { text: "\u00A0", delay: 50 },
+    { text: "\u00A0", delay: 50 },
+    { text: "\u00A0", delay: 50 },
     // ... add more messages with delays
 ];
 
@@ -59,5 +80,7 @@ function displayNextMessage() {
         setTimeout(displayNextMessage, currentMessage.delay || 1000);
     }
 }
+
+var delaydefault = 100 // I want to be able to enable a debug mode so the delays are minimal
 
 displayNextMessage(); // Start displaying messages
